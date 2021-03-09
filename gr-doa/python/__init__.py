607e527e93b5bb0080bb8340b57eda2ad5305b4f
jhunhong <e0318537@u.nus.edu>
2021-03-10 00:47:03 +0800

    porting to Gnuradio 3.8

--- gr-doa/python/__init__.py
+++ gr-doa/python/__init__.py
@@ -22,13 +22,14 @@
 This is the GNU Radio DOA module. Place your Python package
 description here (python/__init__.py).
 '''
+from __future__ import unicode_literals
 
 # import swig generated symbols into the doa namespace
 try:
-	# this might fail if the module is python-only
-	from doa_swig import *
+    # this might fail if the module is python-only
+    from .doa_swig import *
 except ImportError:
-	pass
+    pass
 
 # import any pure python here
 from timestamp_ccf import timestamp_ccf
